eebe4909f7b89b0197979d592a0812930b49c158
Han Yin <naco.siren.2012@gmail.com>
2017-04-16 20:06:34 -0500

    Configured to run on Mac's PyCharm

--- cat.py
+++ cat.py
@@ -1,12 +1,15 @@
 # -*- coding=utf-8 -*-
 import cv2
-# 加载猫脸检测器
+
+# Load the face cascade classifier
 catPath = "haarcascade_frontalcatface.xml"
 faceCascade = cv2.CascadeClassifier(catPath)
-# 读取图片并灰度化
+
+# Read the image and convert to gray scale
 img = cv2.imread("cat1.jpg")  
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-# 猫脸检测
+
+# Detect the cat face
 faces = faceCascade.detectMultiScale(
     gray,
     scaleFactor= 1.02,
@@ -14,11 +17,13 @@ faces = faceCascade.detectMultiScale(
     minSize=(150, 150),
     flags=cv2.CASCADE_SCALE_IMAGE
 )
-# 框出猫脸并加上文字说明
+
+# Draw a rectangle over the cat face and label it
 for (x, y, w, h) in faces:
     cv2.rectangle(img, (x, y), (x+w, y+h), (0, 0, 255), 2)
-    cv2.putText(img,'Cat',(x,y-7), 3, 1.2, (0, 255, 0), 2, cv2.LINE_AA)
-# 显示图片并保存
-cv2.imshow('Cat?', img)
+    cv2.putText(img, 'Cat', (x, y-7), 3, 1.2, (0, 255, 0), 2, cv2.LINE_AA)
+
+# Save the cat image
+#cv2.imshow('Cat?', img)
 cv2.imwrite("cat33.jpg",img)
-c = cv2.waitKey(0)
+#c = cv2.waitKey(0)
